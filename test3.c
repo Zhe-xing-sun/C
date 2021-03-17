af70f501ad7854beb2561ccc9e8bd64b4eb47fb9
佟嘉平 <1569467025@qq.com>
2021-03-17 16:35:42 +0800

    代码提交演示

---
+++ test3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a, b;
+    char c = 0;
+    printf("Enter an expression:");
+    scanf("%f", &a);
+    while (c != '\0') {
+        scanf("%c%f",&c,&b);
+        if (c == '+') {
+            a = a + b;
+        } else if (c == '-') {
+            a = a - b;
+        } else if (c == '*') {
+            a = a * b;
+        } else if (c == '/') {
+            a = a / b;
+        }
+    }
+    printf("Value of expression:%.1f", a);
+    return 0;
+}
+
+
+
+
